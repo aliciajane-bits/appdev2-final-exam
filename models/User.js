8bce1599eee8ce8c23981a2672a8031ac374216b
aliciajane-bits <aliciajanemedina881@gmail.com>
2025-06-07 12:44:59 +0800

    Task 2: File structure and Create user model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+            lowercase: true, // optional: ensures consistent format
+            trim: true
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+    },
+
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
